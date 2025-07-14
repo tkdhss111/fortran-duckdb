455f25d2c39b0f8003ee7183df63081fe19c85f1
Hisashi Takeda <tkdhss111@gmail.com>
2025-07-14 14:59:05 +0900

    tkd2

---
+++ build/test/CMakeFiles/hss_test_duckdb.dir/__/src/duckdb.f90-pp.f90.d
@@ -0,0 +1,2 @@
+test/CMakeFiles/hss_test_duckdb.dir/__/src/duckdb.f90-pp.f90: \
+ /home/hss/0_tkd/1_hss/2_tools/fortran-duckdb/src/duckdb.f90
---
+++ test/01_unit_test_open_close.f90
@@ -0,0 +1,43 @@
+program unit_test_open_close
+  use duckdb_mo
+  implicit none
+  type(duckdb_ty) :: db
+
+  print *, 'Test: Open database: "db.duckdb"'
+  call db%open ( "db.duckdb" )
+  call db%close
+
+  print *, 'Test: Open database'
+  call db%open ( 'db.duckdb' )
+  call db%close
+
+  print *, 'Test: Open database with automatic mode'
+  call db%open ( 'db.duckdb', 'AUTOMATIC' )
+  call db%close
+
+  print *, 'Test: Open database with READ_WRITE mode'
+  call db%open ( 'db.duckdb', 'READ_WRITE' )
+  call db%close
+
+  print *, 'Test: Open in-memory database with READ_ONLY mode'
+  call db%open ( 'db.duckdb', 'READ_ONLY' )
+  call db%close
+
+  print *, 'Test: Open in-memory database'
+  call db%open ( '' )
+  call db%close
+
+  print *, 'Test: Open in-memory database with automatic mode'
+  call db%open ( '', 'AUTOMATIC' )
+  call db%close
+
+  print *, 'Test: Open in-memory database with READ_WRITE mode'
+  call db%open ( '', 'READ_WRITE' )
+  call db%close
+
+  ! This causes error stop
+  print *, 'Test: Open in-memory database with READ_ONLY mode'
+  call db%open ( '', 'READ_ONLY' )
+  call db%close
+
+end program

76366f6cb3a6dfcea4bce46d4d497d21717edf0c
Hisashi Takeda <tkdhss111@gmail.com>
2025-07-14 15:35:13 +0900

    tkd2

--- test/unit_test.f90
+++ test/unit_test.f90
@@ -3,24 +3,28 @@ program unit_test_open_close
   implicit none
   type(duckdb_ty) :: db
 
-  print *, 'Test: Open database: "db.duckdb"'
-  call db%open ( "db.duckdb" )
+  print *, '========================================='
+  print *, ' Open and Close Tests'
+  print *, '========================================='
+
+  print *, 'Test: Open database: "test.duckdb"'
+  call db%open ( "test/test.duckdb" )
   call db%close
 
   print *, 'Test: Open database'
-  call db%open ( 'db.duckdb' )
+  call db%open ( 'test/test.duckdb' )
   call db%close
 
   print *, 'Test: Open database with automatic mode'
-  call db%open ( 'db.duckdb', 'AUTOMATIC' )
+  call db%open ( 'test/test.duckdb', 'AUTOMATIC' )
   call db%close
 
   print *, 'Test: Open database with READ_WRITE mode'
-  call db%open ( 'db.duckdb', 'READ_WRITE' )
+  call db%open ( 'test/test.duckdb', 'READ_WRITE' )
   call db%close
 
   print *, 'Test: Open in-memory database with READ_ONLY mode'
-  call db%open ( 'db.duckdb', 'READ_ONLY' )
+  call db%open ( 'test/test.duckdb', 'READ_ONLY' )
   call db%close
 
   print *, 'Test: Open in-memory database'
@@ -35,9 +39,9 @@ program unit_test_open_close
   call db%open ( '', 'READ_WRITE' )
   call db%close
 
-  ! This causes error stop
-  print *, 'Test: Open in-memory database with READ_ONLY mode'
-  call db%open ( '', 'READ_ONLY' )
-  call db%close
+  ! This causes error stop (error test)
+  !print *, 'Test: Open in-memory database with READ_ONLY mode'
+  !call db%open ( '', 'READ_ONLY' )
+  !call db%close
 
 end program
